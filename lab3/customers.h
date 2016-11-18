8e729b5a3489e6bf67f37a9710e799c126fc5075
aidandonnelly96 <aidandonnelly96@hotmail.com>
2016-11-18 20:32:26 +0000

    Removing redundant files from lab3

--- lab3/customers.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void increment_print(int &x);
-customer *createlist(string name);
-void insertname(customer* head, string name);
-int listlength(customer* head);
-void printcustomers(customer *head);
